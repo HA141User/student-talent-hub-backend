15b7f24c05614d55e7ba30c38e8a24363759113d
Onlyadmirer <akmalrbc6@gmail.com>
2026-06-02 21:10:19 +0800

    feat: update koneksi database

--- app/db/database.py
+++ app/db/database.py
@@ -4,4 +4,5 @@ from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 SessionLocal = async_sessionmaker(autocommit=False, autoflush=False, bind=engine, class_=AsyncSession)
+
 Base = declarative_base()

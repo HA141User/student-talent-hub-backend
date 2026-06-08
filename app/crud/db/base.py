28816f1c0ecfbbc3c48d4b14847b9cc950b20636
HA141User <muhammadhairi0123@gmail.com>
2026-06-08 13:54:08 +0800

    add: kode

---
+++ app/crud/db/base.py
@@ -0,0 +1,6 @@
+from app.crud.db.database import Base
+
+from app.models.user import User
+from app.models.skill import SkillCategory, UserSkill
+from app.models.project import Project, ProjectContributor
+from app.models.endorsement import Endorsement
